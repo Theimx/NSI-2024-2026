c4a4cc05ee22836fc21f5e631a7563e7d8abc6fc
Theimx <85684007+Theimx@users.noreply.github.com>
2025-12-13 10:34:53 +0100

    Add files via upload

---
+++ Epreuve-Ecrite-Bac-NSI.py
@@ -0,0 +1,79 @@
+
+#
+#
+#
+
+
+# A.Sujet-Amérique-du-nord-journée 1-2022-Exercice-4 (4 points)
+
+# B.Sujet-Centres-étrangers-journée-2-2022-Exercice-1 (4 points)
+
+# C.Sujet-Centres-étrangers-journée-2-2022-Exercice-2 (4 points)
+
+# D.Sujet-zéro-2021-Exercice-2 (4 points)
+
+# E.Sujet-métropole-candidat-libre-journée-1-2021-Exercice-4 (4 points)
+
+
+#
+#
+#
+
+
+# A.Sujet-Asie-journée 1-2022-Exercice-4 (4 points)
+
+# B.Sujet-Métropole-journée-1-Remplacement-2021-Exercice-1 (4 points)
+
+# C.Sujet-Polynésie-journée-1-2023-Exercice-2 (5 points)
+
+# D.Sujet-Métropole-journée-2-2022-Exercice-5 (4 points)
+
+# E.Sujet-Centres-étrangers-journée-2-2022-Exercice-4 (4 points)
+
+
+#
+#
+#
+
+
+
+# A.Sujet-Métropole-3-journée 2-2024-Exercice-2 (6 points)
+
+def produire_jeu(n):
+    resultat = Pile()
+    for i in range(...):
+        resultat.empiler(...)
+    return resultat
+
+# B.Sujet-Centre-étranger-1-journée 1-2022-Exercice-2 (4 points)
+
+# C.Sujet-Centre-étranger-journée 1-2021-Exercice-5 (4 points)
+import random 
+
+def ajout(f):
+    couleurs = ("bleu","rouge","jaune","vert")
+    indice = randint(0,3)
+    enfiler(f,couleurs[indice])
+    return f
+
+def vider(f):
+    while not f.est_vide() : 
+        f.defiler()
+
+def affich_seq(sequence):
+    stock = []
+
+def tour_de_jeu(sequence):
+
+    stock = creer_file_vide()
+    while not est_vide(sequence) : 
+        c_joueur = saisie_joueur()
+        c_seq = None
+
+# D.Sujet-Asie-journée 2-2024-Exercice-2 (6 points)
+
+    # 1 : l'Expression  
+
+#A. Sujet Amérique du sud -journée 2-2022-Exercice 1 (4 points sur 12)
+
+#B
